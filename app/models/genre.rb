92cdc8d066e206193499fad97fd6f99911287b16
Elena-Weber <crochetmiracles@gmail.com>
2021-04-23 14:49:50 -0400

    commit tree c8caf4c58f4898a26db361e914815273baac9e27

---
+++ app/models/genre.rb
@@ -0,0 +1,6 @@
+class Genre < ApplicationRecord
+
+    has_many :songs
+    has_many :genres, through: :songs
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+5.times do
+    Artist.create(
+    name: Faker::Music.band,
+    bio: Faker::Quote.famous_last_words
+    )
+end
+
+3.times do
+    Genre.create(
+    name: Faker::Music.genre
+)
+end
+
+15.times do
+    Song.create(
+    name: Faker::Quote.singular_siegler,
+    artist_id: rand(1..5),
+    genre_id: rand(1..3)
+    )
+end
+
+puts "Success"
